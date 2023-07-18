49e75c004eeb19f637a4c7e5d867cb6db0a3ad1e
nshimiyimano-cloud <njeanluc828@gmail.com>
2023-07-18 20:59:54 +0200

    After instal Ngbootstra,added navbar,anvironment firbase

---
+++ src/environment/development.ts
@@ -0,0 +1,16 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+    production: false,
+    firebase:{
+        apiKey: "AIzaSyDwxfPD63zaS0FEQ17SAlVMjflWxkm7FUA",
+        authDomain: "organicshop-3cadf.firebaseapp.com",
+        projectId: "organicshop-3cadf",
+        storageBucket: "organicshop-3cadf.appspot.com",
+        messagingSenderId: "1046959420893"
+    }
+  };
+  
+  
